0b786358aa427cfbe432ee96954c0a0ceea94c12
Max <maxine.micu@gmail.com>
2020-07-12 10:40:48 +0800

    feat: pass medicine, onclick to MedicineItem component

--- components/item_medicine.tsx
+++ components/item_medicine.tsx
@@ -1,30 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {stylesText, stylesView} from '../resources/styles'
+import { stylesText, stylesView } from '../resources/styles'
 import {
-    View, Text, Button, Image
+    View, Text, Button, Image, TouchableOpacity
 } from 'react-native';
-import {colors} from '../resources/colors'
+import { Medicine } from '../state/type';
 
-export function MedicineItem() {
+export function MedicineItem(medicine: Medicine, onClick: () => void) {
     return (
-        <View style={[stylesView.itemContainer, {flexDirection: 'row'}]}>
-            <View style={{justifyContent: 'center'}}>
-                <Image
-                    source={require('../assets/capsule.png')}
-                    style={{width: 40, height: 40, marginRight: 10}}>
-                </Image>
+        <TouchableOpacity onPress={onClick}>
+            <View style={[stylesView.itemContainer, { flexDirection: 'row' }]}>
+                <View style={{ justifyContent: 'center' }}>
+                    <Image
+                        source={require('../assets/capsule.png')}
+                        style={{ width: 40, height: 40, marginRight: 10 }}>
+                    </Image>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+                    <Text style={stylesText.title}>{medicine.name}</Text>
+                    <Text style={stylesText.small}>{medicine.brand}</Text>
+                    <Text style={stylesText.small}>{medicine.volume}</Text>
+                </View>
+                <View style={[{ justifyContent: 'center' }]}>
+                    <Text style={[stylesText.smallBold, stylesView.expirationContainer]}>{medicine.expirationDate}</Text>
+                </View>
             </View>
-            <View style={{ flex: 1, flexDirection: 'column', justifyContent:'center'}}>
-                <Text style={stylesText.title}>Title</Text>
-                <Text style = {stylesText.small}>Brand</Text>
-                <Text style = {stylesText.small}>Volume</Text>
-            </View>
-            <View style={[{justifyContent:'center'}]}>
-                <Text style = {[stylesText.smallBold, stylesView.expirationContainer]}>Expiration</Text>
-            </View>
-        </View>
+        </TouchableOpacity>
     )
 }
+
+MedicineItem.prototype = {
+    onClick: PropTypes.func.isRequired,
+    medicine: PropTypes.object.isRequired
+}
+
 
 
